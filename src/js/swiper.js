2313b672f29685799888516583eda51473a74a3d
Olena Shatalova <08082023m@gmail.com>
2025-03-16 23:37:16 +0200

    added reviews swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,12 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
-import { Scrollbar } from 'swiper/modules';
+import { Scrollbar, Autoplay } from 'swiper/modules';
 
 // Swiper for Gallery Section
 document.addEventListener('DOMContentLoaded', () => {
   const gallerySwiper = new Swiper('.gallery-swiper', {
-    modules: [Scrollbar],
+    modules: [Scrollbar, Autoplay],
     scrollbar: {
       el: '.gallery-swiper .swiper-scrollbar',
       draggable: true,
@@ -14,6 +14,38 @@ document.addEventListener('DOMContentLoaded', () => {
     slidesPerView: 'auto',
     spaceBetween: 48,
     centeredSlides: false,
+    loop: false,
+    speed: 1000,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    on: {
+      reachEnd: function () {
+        setTimeout(() => {
+          this.slideTo(0);
+        }, 3000);
+      },
+    },
+  });
+
+  // Swiper for Reviews Section
+  const reviewsSwiper = new Swiper('.reviews-swiper', {
+    modules: [Scrollbar, Autoplay],
+    scrollbar: {
+      el: '.reviews-swiper .swiper-scrollbar',
+      draggable: true,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    centeredSlides: false,
+    loop: false,
+    speed: 1000,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
     on: {
       reachEnd: function () {
         setTimeout(() => {
@@ -22,18 +54,6 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     },
 
-    loop: false,
-  });
-
-  // Swiper for Reviews Section
-  const reviewsSwiper = new Swiper('.reviews-swiper', {
-    modules: [Scrollbar],
-    scrollbar: {
-      el: '.reviews-swiper .swiper-scrollbar',
-      draggable: true,
-    },
-    loop: false,
-    slidesPerView: 1.2,
     breakpoints: {
       1200: {
         slidesPerView: 4,
