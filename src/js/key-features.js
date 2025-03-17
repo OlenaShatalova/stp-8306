d14aefb2ad68e63535e37eca33c50ba3babae8d8
Rodika Chorba <158561902+RodikaChorba@users.noreply.github.com>
2025-03-17 14:01:15 +0100

    key-features17-1

--- src/js/key-features.js
+++ src/js/key-features.js
@@ -1,17 +1,28 @@
 const keyFeaturesButtons = document.querySelectorAll('.key-features__button');
+const blueBlock = document.querySelector('.key-features__blue-block');
 
 keyFeaturesButtons.forEach(button => {
   button.addEventListener('click', () => {
     const contentId = button.dataset.content;
     const content = document.getElementById(contentId);
 
-    // Закриваються всі відкриті блоки
+    // Закриваємо всі відкриті блоки
     document.querySelectorAll('.key-features__text.active').forEach(item => {
       if (item !== content) {
         item.classList.remove('active');
+        item.parentElement.querySelector('.key-features__button').style.height =
+          '98px';
       }
     });
 
-    content.classList.toggle('active');
+    if (content.classList.contains('active')) {
+      content.classList.remove('active');
+      button.style.height = '98px';
+      blueBlock.style.height = '816px'; // Повертаємо початкову висоту синього блоку
+    } else {
+      content.classList.add('active');
+      button.style.height = '208px';
+      blueBlock.style.height = '1724px'; // Змінюємо висоту синього блоку
+    }
   });
 });
