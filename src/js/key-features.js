95285ab9e1bfbfbdcb8bc36ca45c3b452ee8b9db
Olena Shatalova <08082023m@gmail.com>
2025-03-21 23:11:24 +0200

    added keyFeatures animation

--- src/js/key-features.js
+++ src/js/key-features.js
@@ -1,9 +1,28 @@
 const keyFeaturesButtons = document.querySelectorAll('.key-features__button');
+const star = document.querySelector('.key-features__star');
+const starBlock = document.querySelector('.star-block');
+const firstItem = document.querySelector('.key-features__item');
+
+console.log(star, starBlock);
 
 keyFeaturesButtons.forEach(button => {
   button.addEventListener('click', () => {
     const item = button.closest('.key-features__item');
-
     item.classList.toggle('openedBlock');
   });
 });
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          firstItem.classList.add('openedBlock');
+        }, 1000);
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+observer.observe(starBlock);
